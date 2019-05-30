701b5bfe7411e630946fdb02b09794d533a644fe
degarashi <slice013@gmail.com>
2019-05-30 23:44:04 +0900

    [add]Qt5のビルドに使うマクロ

---
+++ qt_aux.cmake
@@ -0,0 +1,38 @@
+# Qt5のセットアップルーチンなど
+
+macro(BuildWithQt5 SRC PROJNAME)
+	set(Lcl_Opt_Flags "")
+	set(Lcl_Opt_Single "")
+	set(Lcl_Opt_Multi UI_FILE TS_FILE MODULE)
+	cmake_parse_arguments(BQt5 "${Lcl_Opt_Flags}" "${Lcl_Opt_Single}" "${Lcl_Opt_Multi}" ${ARGN})
+
+	set(CMAKE_AUTOMOC ON)
+	set(CMAKE_AUTOUIC ON)
+	set(CMAKE_INCLUDE_CURRENT_DIR ON)
+
+	find_package(Qt5 COMPONENTS
+		${BQt5_MODULE}
+		REQUIRED
+	)
+	qt5_create_translation(QM_FILES
+		"${SRC};${BQt5_UI_FILE}"
+		${BQt5_TS_FILE}
+	)
+	add_executable(
+		${PROJNAME}
+		${SRC}
+		${BQt5_UI_FILE}
+		${QM_FILES}
+	)
+	# LinguistTools以外をリストアップ
+	foreach(m IN LISTS BQt5_MODULE)
+		if(NOT m STREQUAL "LinguistTools")
+			list(APPEND Lcl_LinkLibs "Qt5::${m}")
+		endif()
+	endforeach()
+	target_link_libraries(
+		${PROJNAME}
+		${Lcl_LinkLibs}
+		${CMAKE_THREAD_LIBS_INIT}
+	)
+endmacro()
